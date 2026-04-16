d7d6b5e0efda79f3e5d307aaf9209b681d1b0369
Natalia Valencia <natalia_valencia@Natalias-MacBook-Air.local>
2026-04-16 00:58:37 -0400

    comienzo actividad 2 - DNS

--- control1/actividad-dns/resolver.py
+++ control1/actividad-dns/resolver.py
@@ -1,11 +1,7 @@
-# Cree un archivo llamado resolver.py.
-# En él construya un código que le permita obtener mensajes DNS.
-#  Para esto cree un socket apropiado que se encuentre asociado a la dirección (IP_VM, 8000)
-# (¿Qué tipo de socket debe usar? Anótelo en su informe).
-# Luego haga que su socket pueda recibir mensajes en un loop, aquí puede utilizar un tamaño de
-# buffer "grande" pues en esta actividad no nos preocuparemos de manejar mensajes más grandes
-#  que el tamaño del buffer.
-# Finalmente haga que su código imprima en pantalla el mensaje recibido tal como se recibió, es decir, no utilice decode().
+import socket
+
+import dnslib
+from dnslib import DNSRecord
 
 # Natalia:
 # IP VM: 192.168.64.3
@@ -13,3 +9,38 @@
 
 IP_HOST = "192.168.64.1"
 IP_VM = "192.168.64.3"
+# ========================================== funcion extra ==========================================
+# Programe una función para parsear mensajes DNS. Es decir, programe una función que sea
+# capaz de tomar un mensaje DNS y transformarlo a alguna estructura de datos manejable.
+# Para esta parte podrá usar tanto hexadecimal con binascii como dnslib. Para crear sus
+# funciones puede usar como guía la información y código presentado en la sección anterior
+# Programando DNS con sockets.
+# Si decide utilizar dnslib se recomienda revisar el material provisto más abajo en la sección
+# Material e indicacciones para la actividad.
+# Nota: Se recomienda guardar la siguiente información en su estructura:
+# Qname, ANCOUNT, NSCOUNT, ARCOUNT, la sección Answer, la sección Authority y la sección Additional.
+# =====================================================================================================
+# creamos un socket no orientado a conexión
+print(" ... Creando Socket ...\n")
+socket_resolver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket_address = (IP_HOST, 8000)
+
+# recordar descargar dnslib con pip install dnslib
+
+# Al igual que los servidores HTTP, los servidores DNS ocupan un puerto reservado,
+# en este caso este corresponde al puerto 53.
+
+if __name__ == "__main__":
+    buffer_size = 4096
+    # la idea es conectar el socket a (IP_VM, 8000)
+    socket_resolver.bind(socket_address)
+    print(f"Socket de DNS escuchando en {socket_address}\n")
+
+    while True:
+        message_bytes, client_addres = socket_resolver.recvfrom(buffer_size)
+        print(f"Longitud del mensaje: {len(message_bytes)}\n")
+        print(f"Mensaje recibido: \n{message_bytes}\n")
+        break
+
+    print(" ... Cerrando socket DNS ... ")
+    socket_resolver.close()
