bade4fd3343f27e3482ad5ff5d976f182ba5f609
Natalia Valencia <natalia_valencia@Natalias-MacBook-Air.local>
2026-04-17 04:40:40 -0400

    resolver sin probar

--- control1/actividad-dns/resolver.py
+++ control1/actividad-dns/resolver.py
@@ -13,7 +13,6 @@ IP_HOST = "192.168.64.1"
 IP_VM = "192.168.64.3"
 
 
-# ========================================== funcion extra ==========================================
 # Programe una función para parsear mensajes DNS, que sea capaz de tomar un mensaje DNS y transformarlo
 # a alguna estructura de datos manejable.
 # Programando DNS con sockets.
@@ -89,56 +88,117 @@ def send_dns_query(query, ip):
     return response_bytes
 
 
+def get_rdata(parsed_msg):
+    for ans in parsed_msg["answers"]:
+        if ans["type"] == "A":
+            return ans["rdata"]
+    return None
+
+
+def is_there_answer_type_a(parsed_msg, section):
+    is_there_answer = False
+    for ans in parsed_msg[section]:
+        if ans["type"] == "A":
+            is_there_answer = True
+    return is_there_answer
+
+
+def resolver_ns_recursivo(ns, msg_consulta):
+    ns_query = DNSRecord.question(ns)
+    ns_bytes = ns_query.pack()
+    ns_response_bytes = resolver(ns_bytes)
+    if ns_response_bytes:
+        ns_parsed = parser_dns_message(ns_response_bytes)
+        ns_ip = None
+        for a in ns_parsed["answers"]:
+            if a["type"] == "A":
+                ns_ip = a["rdata"]
+                break
+        return ns_ip
+
+
 # recibe el mensaje de query en bytes obtenido desde el cliente. Dentro de esta función, siga el siguiente
 # procedimiento para obtener la respuesta:
 def resolver(mensaje_consulta):
     # Envíe el mensaje query al servidor raíz de DNS y espere su respuesta. Se recomienda dejar la IP del
     # servidor raíz en una variable global de su programa.
-    print(" .. Consultando servidor raiz ... ")
+    print(" .. Consultando servidor raiz ...\n")
     respuesta_bytes = send_dns_query(mensaje_consulta, SERVER_ADDRESS)
-    respuesta = DNSRecord.parse(respuesta_bytes)
 
-    ancount = respuesta.header.a
-    nscount = respuesta.header.auth
-    arcount = respuesta.header.ar
+    if not respuesta_bytes:
+        print("No se recibió respuesta")
+        return None
 
-    # Si el mensaje answer recibido tiene la respuesta a la consulta, es decir, viene alguna respuesta de tipo A en
-    # la sección Answer del mensaje, entonces simplemente haga que su función retorne el mensaje recibido.
-    if ancount > 0:
-        for rr in respuesta.rr:
-            if rr.rtype == QTYPE.A:
-                print(f"Respuesta encontrada: \n{rr.rdata}\n")
-                return respuesta_bytes
+    # Parseamos la respuesta a la estructura de diccionario
+    respuesta_parsed = parser_dns_message(respuesta_bytes)
 
-    # Si la respuesta recibida corresponde a una delegación a otro Name Server, es decir, vienen respuestas de tipo
-    # NS en la sección Authority, revise si viene alguna respuesta de tipo A en la sección Additional.
-    if nscount > 0:
-        print("... delegación a otro Name Server ...\n")
-        ips_additional = []
-        if arcount > 0 and hasattr(respuesta, "ar"):
-            for rr in respuesta.ar:
-                if rr.rtype == QTYPE.A:
-                    ip = str(rr.rdata)
-                    ips_additional.append(ip)
-                    print(f"Encontramos IP en Additional: {ip}\n")
-        #  i.- Si encuentra una respuesta tipo A, entonces envíe la query del paso a) a la primera dirección IP
-        #      contenida en la sección Additional.
-        if ips_additional:
-            print(" ... Enviando consulta a Name Server ...\n")
-            first_ip_address = ips_additional[0]
-            respuesta_bytes = send_dns_query(mensaje_consulta, first_ip_address)
-            print(f"{respuesta_bytes}\n")
-            return None
+    # Si el mensaje answer recibido tiene la respuesta a la consulta, es decir, viene alguna respuesta
+    # de tipo A en la sección Answer del mensaje, entonces simplemente haga que su función retorne el
+    # mensaje recibido.
 
-        #  ii.- En caso de no encontrar alguna IP en la sección Additional, tome el nombre de un Name Server desde la
-        #       sección Authority y use recursivamente su función para resolver la IP asociada al nombre de dominio del
-        #       Name Server. Una vez obtenga la IP del Name Server, envíe la query obtenida en el paso a) a dicha IP.
-        #       Una vez recibida la respuesta, vuelva al paso b).
-        else:
-            print("... No hay IP en sección Additional ...\n")
+    # vemos si hay respuesta tipo A en la seccion Answer
 
+    if respuesta_parsed["ancount"] > 0 and is_there_answer_type_a(
+        respuesta_parsed, "answers"
+    ):
+        ip = get_rdata(respuesta_parsed)
+        print(f"Encontrado {ip}\n")
+        return response_bytes
+
+    # Si la respuesta recibida corresponde a una delegación a otro Name Server, es decir, vienen
+    # respuestas de tipo NS en la sección Authority, revise si viene alguna respuesta de tipo A en
+    # la sección Additional.
+    if respuesta_parsed["nscount"] > 0:
+        print("... Delegación a otro Name Server... \n")
+        is_there_answer = is_there_answer_type_a(respuesta_parsed, "additional")
+        # Si encuentra una respuesta tipo A, entonces envíe la query del paso a) a la primera
+        # dirección IP contenida en la sección Additional.
+        if is_there_answer:
+            # buscamos direccion IP en Additional
+            ip_additional = []
+            for add in respuesta_parsed["additional"]:
+                if add["type"] == "A":
+                    ip_additional.append(add["rdata"])
+
+            if ip_additional:
+                # Si encuentra una respuesta tipo A, entonces envíe la query del paso a) a la primera
+                # dirección IP contenida en la sección Additional
+                first_ip = ip_additional[0]
+                print(f"... Consulta con IP de sección Additional: {first_ip} ...\n")
+                respuesta_bytes = send_dns_query(mensaje_consulta, first_ip)
+                if respuesta_bytes:
+                    respuesta_parsed = parser_dns_message(respuesta_bytes)
+                    if respuesta_parsed["ancount"] > 0 and is_there_answer_type_a(
+                        respuesta_parsed, "answers"
+                    ):
+                        return respuesta_bytes
+                    else:
+                        resolver(respuesta_bytes)
+            else:
+                # Si no, tomamos el nombre de un Name Server desde la sección Authority y use recursivamente su función
+                # para resolver la IP asociada al nombre de dominio del Name Server
+                print("... No hay IP en Additional ...\n")
+                ns_list = []
+                for auth in respuesta_parsed["authority"]:
+                    if auth["type"] == "NS":
+                        ns_list.append(auth["rdata"])
+
+                for ns in ns_list:
+                    # hay que resolver la IP asociada recursivamente
+                    ns_ip = resolver_ns_recursivo(ns, mensaje_consulta)
+                    if ns_ip:
+                        # enviar consulta
+                        respuesta_bytes = send_dns_query(mensaje_consulta, ns_ip)
+                        if respuesta_bytes:
+                            respuesta_parsed = parser_dns_message(respuesta_bytes)
+                            if is_there_answer_type_a(respuesta_parsed, "answer"):
+                                return respuesta_bytes
+                            else:
+                                resolver(respuesta_bytes)
+                        else:
+                            print("No hay respuesta\n")
+                    break
     # Si recibe algún otro tipo de respuesta simplemente ignórela
-    print("Respuesta sin delegacion ni respuesta ")
     return None
 
 
